1ddfb97cb50d5b6fa4f9a61ffea55b246d99ad8f
animesh global <Animesh.Tripathi@radisys.com>
2023-04-28 15:45:20 +0530

    created copy of main.html

---
+++ app.js
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>Javascript Main Page</h2>
+
+<p id="main">Javascript changes</p>
+<button type="button" onClick='document.getElementById("main").innerHTML="Hey! user welcome"'>Submit</button>
+</body>
+</html>
